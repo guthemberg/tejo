cb4cd05b31dd668bc1a90997295a7f8d4da3bc08
guthemberg <guthemberg.silvestre@gmail.com>
2015-06-22 15:49:30 +0200

    go

--- contrib/debian/mongodb/setup_repset.py
+++ contrib/debian/mongodb/setup_repset.py
@@ -30,6 +30,7 @@ def main(argv):
         host="%s:27017" % hostname
         hosts.append({'_id': id, 'host': host})
         id=id+1
+    print hosts
     config={'_id': rset, 'members': hosts}
     c = MongoClient('localhost', 27017)
     c.admin.command("replSetInitiate", config)
