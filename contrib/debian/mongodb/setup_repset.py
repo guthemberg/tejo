1ff030dfc228f9a37fb7c45a54510e0939025b58
guthemberg <guthemberg.silvestre@gmail.com>
2015-06-25 15:27:36 +0200

    again

--- contrib/debian/mongodb/setup_repset.py
+++ contrib/debian/mongodb/setup_repset.py
@@ -6,27 +6,30 @@ import sys, getopt
 def compute_args(argv):
    rset = ''
    vms = ''
+   arbiter=''
    
    try:
-       opts, args = getopt.getopt(argv,"hn:v:",["rset=","vms="])
+       opts, args = getopt.getopt(argv,"hn:v:a:",["rset=","vms=","arbiter="])
    except getopt.GetoptError:
-       print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...>'
+       print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...> -a arbiter'
        sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...>'
+         print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...> -a arbiter'
          sys.exit()
       elif opt in ("-n", "--rset"):
          rset = arg
       elif opt in ("-v", "--vms"):
          vms = arg
-   if len(rset)==0 or len(vms)==0:
-       print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...>'
+      elif opt in ("-a", "--arbiter"):
+         arbiter = arg
+   if len(rset)==0 or len(vms)==0 or len(arbiter)==0:
+       print 'setup_repset.py -n <rep_set> -v <vm1,vm2,...> -a arbiter'
        sys.exit(2)
-   return (rset,vms)
+   return (rset,vms,arbiter)
    
 def main(argv):
-    (rset,vms)=compute_args(argv)
+    (rset,vms,arbiter)=compute_args(argv)
     
     hosts=[]
     id=0
@@ -34,6 +37,9 @@ def main(argv):
         host="%s:27017" % hostname
         hosts.append({'_id': id, 'host': host})
         id=id+1
+    #arbiter
+    host="%s:30000" % arbiter
+    hosts.append({'_id': id, 'host': host,"arbiterOnly" : True})
     config={'_id': rset, 'members': hosts}
     c = MongoClient('localhost', 27017)
     c.admin.command("replSetInitiate", config)
