fdc49132e9526706b9554a4a0d7e3e2789ede3d7
guthemberg <guthemberg.silvestre@gmail.com>
2015-07-12 15:52:08 +0200

    upload

--- contrib/debian/mongodb/load.sh
+++ contrib/debian/mongodb/load.sh
@@ -1,7 +1,11 @@
+. /etc/tejo.conf
+
 #echo -n "cleaning up... "
 
 #sh /home/user/svc/experiments_scripts/ycsb/stop.sh;
 #echo "done."
+
+sed "s|RECORDCOUNT|$mongo_recordcount|g" /home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/workloads/load > /tmp/myload
 
 sudo chmod ugo+x /home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/bin/ycsb
 
@@ -9,6 +13,6 @@ host_name=`hostname`
 
 echo -n "loading new mongodb... "
 
-/home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/bin/ycsb load mongodb -p mongodb.url="mongodb://${host_name}:27017" -p mongodb.database="ycsb" -p mongodb.writeConcern="normal" -p mongodb.maxconnections=1000 -threads 40 -s -P /home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/workloads/load
+/home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/bin/ycsb load mongodb -p mongodb.url="mongodb://${host_name}:27017" -p mongodb.database="ycsb" -p mongodb.writeConcern="normal" -p mongodb.maxconnections=1000 -threads 40 -s -P /tmp/myload
 
 echo "done."
