15c54a3065d1424b7d3a0f59b1030175bddf15ad
guthemberg <guthemberg.silvestre@gmail.com>
2015-07-14 17:38:27 +0200

    hard

--- contrib/debian/mongodb/load.sh
+++ contrib/debian/mongodb/load.sh
@@ -13,6 +13,9 @@ host_name=`hostname`
 
 echo -n "loading new mongodb... "
 
-/home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/bin/ycsb load mongodb -p mongodb.url="mongodb://${host_name}:27017" -p mongodb.database="ycsb" -p mongodb.writeConcern="normal" -p mongodb.maxconnections=500 -threads 20 -s -P /tmp/myload
+#mongodb.maxconnections=1000 -threads 80
+#mongodb.maxconnections=2000 -threads 200 
+#mongodb.maxconnections=3000 -threads 300
+/home/user/tejo/contrib/debian/mongodb/ycsb-0.1.4/bin/ycsb load mongodb -p mongodb.url="mongodb://${host_name}:27017" -p mongodb.database="ycsb" -p mongodb.writeConcern="normal" -p mongodb.maxconnections=2000 -threads 200 -s -P /tmp/myload
 
 echo "done."
