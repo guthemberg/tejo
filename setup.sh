cc171f464a3a2e5e7518e3d1878e5fe80ed20d3b
guthemberg <guthemberg.silvestre@gmail.com>
2015-07-23 15:18:52 +0200

    setup script skeleton

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+##this script implements the steps to install a role in a node and are specific 
+#to the topology tejo cluster
+##follow the deliverable to complete this script accordingly
+
+
+#main
+role=$1
+git clone https://github.com/guthemberg/tejo.git
