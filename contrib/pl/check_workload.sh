cba2739d9109c07facdb06d40e941557c9a4f43a
guthemberg <guthemberg.silvestre@gmail.com>
2015-08-07 22:13:04 +0200

    fort

---
+++ contrib/pl/check_workload.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+. /etc/tejo.conf
+
+if [ -e $mongo_active_wl_file -a `pgrep -f ycsb-0.1.4` -eq 0 ]
+then
+	sh ${home_dir}/tejo/common/experiments_scripts/ycsb/run.sh $system_id $mongo_default_throughput &
+fi
--- tejo/common/experiments_scripts/ycsb/stop.sh
+++ tejo/common/experiments_scripts/ycsb/stop.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+
+
+
+#how to call is
+# sh run.sh
+# or
+# sh run.sh SYSTEM_ID OPERATIONS_PER_SECOND
+#where 	SYSTEM_ID might be 0 or 2
+#		OPERATION PER SECOND 50
+. /etc/tejo.conf
+
+
+rm $mongo_active_wl_file
+
 /usr/bin/pkill -f run.sh ; /usr/bin/pkill -f ycsb-0.1.4 ; /bin/rm -rf /tmp/slo_*; cd /home/`whoami`; sudo rm -rf /tmp/experiment_outputs ; sudo mkdir /tmp/experiment_outputs; sudo chown `whoami` /tmp/experiment_outputs 
