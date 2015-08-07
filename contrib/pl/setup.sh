39346dd50ea8d632551fc799940d76dc8ade7cbe
guthemberg <guthemberg.silvestre@gmail.com>
2015-08-07 16:03:08 +0200

    set

--- contrib/pl/setup.sh
+++ contrib/pl/setup.sh
@@ -17,4 +17,5 @@ location=`get_host_location`
 
 ssh -i ${root_dir}/.ssh/id_rsa_cloud -o StrictHostKeyChecking=no -t $workload_user@$peer "wget --no-check-certificate https://raw.githubusercontent.com/guthemberg/yanoama/master/contrib/tejo/setup.sh -O /tmp/setup.sh"
 ssh -i ${root_dir}/.ssh/id_rsa_cloud -o StrictHostKeyChecking=no -t $workload_user@$peer "sh /tmp/setup.sh -l $location -d $default_domain -t workload -a $me"
+ssh -i ${root_dir}/.ssh/id_rsa_cloud -o StrictHostKeyChecking=no -t $workload_user@$peer "sh /home/${workload_user}/tejo/tejo/common/experiments_scripts/ycsb/stop.sh"
 ssh -i ${root_dir}/.ssh/id_rsa_cloud -o StrictHostKeyChecking=no -t $workload_user@$peer "sh /home/${workload_user}/tejo/tejo/common/experiments_scripts/ycsb/run.sh 0 300 &"
