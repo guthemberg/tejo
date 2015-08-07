39e82ebded74a8a16baab4d04140fa1802e73328
guthemberg <guthemberg.silvestre@gmail.com>
2015-08-07 17:34:46 +0200

    comm

--- tejo/common/experiments_scripts/peers/refresh_workload_wrapper.sh
+++ tejo/common/experiments_scripts/peers/refresh_workload_wrapper.sh
@@ -4,4 +4,8 @@
 
 pkill -f refresh_workload_list.py
 /bin/tcsh -c "python ${home_dir}/tejo/common/experiments_scripts/peers/refresh_workload_list.py  >& /tmp/refresh_workload.log"
-sudo cp $monitors_list_file $www_dir
+python -c "import pickle ; print pickle.load( open( $monitors_list_file, 'rb' ) )"
+if [ $? -eq 0 ]
+then
+	sudo cp $monitors_list_file $www_dir
+fi
